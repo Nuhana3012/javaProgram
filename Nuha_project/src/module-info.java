1326de2c42fb09ff2800f4abc16277176ae92fd8
Nuhana3012 <nuhanaakbar3012@gmail.com>
2025-08-05 08:48:25 +0530

    initial commit

---
+++ Nuha_project/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Nuha_project {
+}
---
+++ Nuha_project/src/program1/Nuhana.java
@@ -0,0 +1,11 @@
+package program1;
+
+public class Nuhana {
+
+	public static void main(String[] args) 
+	{
+		System.out.println();
+
+	}
+
+}
